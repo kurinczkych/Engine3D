fe527bf2888cff6733f80761d41f160db49a5bb4
Chloe <kurinczkydd@gmail.com>
2023-09-08 17:35:56 +0200

    Add project files.

---
+++ Mario64/Engine.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+namespace Mario64
+{
+    public class Engine : GameWindow
+    {
+        float[] verticies =
+        { 
+            0f, 0.5f, 0f,
+            -0,5f, -0.5f, 0f,
+            0.5f, -0.5f, 0f
+        };
+
+        //Render pipeline vars
+        int vao;
+        int shaderProgram;
+
+        private int ScreenWidth;
+        private int ScreenHeight;
+
+        public Engine(int width, int height) : base(GameWindowSettings.Default, NativeWindowSettings.Default)
+        {
+            this.CenterWindow(new Vector2i(width, height));
+            ScreenWidth = width;
+            ScreenHeight = height;
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs args)
+        {
+            GL.ClearColor(Color4.LightBlue);
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            Context.SwapBuffers();
+
+            base.OnRenderFrame(args);
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs args)
+        {
+            base.OnUpdateFrame(args);
+        }
+
+        protected override void OnLoad()
+        {
+            base.OnLoad();
+
+            vao = GL.GenVertexArray();
+
+            int vbo = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ArrayBuffer, vbo);
+
+            GL.BufferData(BufferTarget.ArrayBuffer, verticies.Length*sizeof(float), verticies, BufferUsageHint.StaticDraw);
+
+            GL.BindVertexArray(vao);
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 0, 0);
+            GL.EnableVertexArrayAttrib(vao, 0);
+
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+            GL.BindVertexArray(0);
+        }
+
+        protected override void OnUnload()
+        {
+            base.OnUnload();
+        }
+    }
+}
---
+++ Mario64/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Mario64
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using(Engine engine = new Engine(500,500))
+            {
+                engine.Run();
+            }
+        }
+    }
+}
