ec90e099021636b9021a816031b05cbdcfd8e668
kurinczkydd@stud.uni-obuda.hu <kurinczkydd@stud.uni-obuda.hu>
2024-10-07 12:17:14 +0200

    eh

---
+++ Editor3D/Editor.cs
@@ -0,0 +1,13 @@
+﻿using Engine3D;
+
+namespace Editor3D
+{
+    internal class Editor
+    {
+        static void Main(string[] args)
+        {
+            Engine engine = new Engine(1280, 768);
+            engine.Run();
+        }
+    }
+}
